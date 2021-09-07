579138f3032ee7021ae25e0e1e11755aaab00472
Joshua Palathinkal <jpalathinkal@usf.edu>
2021-09-07 00:39:17 -0400

    Add project files.

---
+++ Tech Assignment 1- Simple Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_1__Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
